d453440c613632d25d101a5ea126ce35754de05d
Mac Dowell Prosper <macdowell@noukod.com>
2019-03-15 15:00:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(drivers){
-    return drivers.map(function (driver){return emp.toLowerCase()});
+    return drivers.map(function (driver){return driver.toLowerCase()});
 }
 
 function nameToAttributes(drivers) {
-    return drivers.map(function (emp){const fullName = emp.split(' '); return {firstName: fullName[0], lastName: fullName[1]};})
+    return drivers.map(function (driver){const fullName = driver.split(' '); return {firstName: fullName[0], lastName: fullName[1]};})
 }
 
 function attributesToPhrase(drivers){
-    return drivers.map(function (emp){
-        return `${emp.name} is from ${emp.hometown}`;
+    return drivers.map(function (driver){
+        return `${driver.name} is from ${driver.hometown}`;
     });
 }
